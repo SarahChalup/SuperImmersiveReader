0cf0999d7f8417b5feab415857b04154b32d6751
Sarah Chalup Roca <a2018112520@estudiantes.upsa.edu.bo>
2021-05-25 18:19:44 -0400

    Completado

---
+++ InmersiveReaderWA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InmersiveReaderWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
